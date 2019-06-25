63c324e5d6a163957ad83ee578693eb2f13a6498
Bernardo FajardoMaldonado <ironcolt@gmail.com>
2019-06-25 16:03:14 -0500

    Improve the installation process
    Adds a function to install every app and link sent from the script body,
    it handles errors produced by the system if failing to install the
    requested app.
    Changes the link to the repo needed to install Fedy, since the old
    version is not longer valid.
    Also, it adds minor text changes to improve and make the script clearer
    for reading.

--- Fedora_Configuration_Update.sh
+++ Fedora_Configuration_Update.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-##### Fedora_26_27_Configuration_Update
+##### Fedora_Configuration_Update
 
-##### Script to install the basic configuration for Fedora 26/27
-##### It must be run as root or sudo privileges
-##### It must be executed only once, after the manual basic configuration done after a fresh installation
+##### Script to install the basic configuration for Fedora
+##### It must be run as root or with sudo privileges
+##### It must be executed when a manual basic configuration after a fresh installation has been done
+
 
 ##### Variables
 install="install -y"
 update="update -y"
 input=""
 vm=0
+
+
+##### Functions
+function Install() {
+	app_name="$1"; echo "Installing ""$app_name"
+	echo
+	sudo dnf $install "$1"; error=$( echo $? )
+	if [[ $error == 1 ]]; then echo; echo "ERROR installing ""$app_name"; echo; echo; fi
+  echo
+	echo
+}
+
+
 
 ##### Begin
 clear
@@ -33,72 +47,79 @@ case $input in
     * )		vm=0;;
 esac
 
+
 echo
 echo "##### Cleaning files in dnf cache..."
 dnf clean all
 echo "##### Done #####"
 echo
 
+
 echo "##### Modifying dnf.config file..."
-###	Modify dnf config file
 echo "fastestmirror=true" >> /etc/dnf/dnf.conf
 echo "deltarpm=false" >> /etc/dnf/dnf.conf
 echo "##### Done #####"
 echo
 
-echo "##### Installing repos..."
-###	Package managers and repos
-dnf $install wget
-dnf $install curl
-dnf $install dnf-plugins-core
-dnf $install yumex-dnf
+
+echo "##### Installing repos and basic utilities..."
 ### RPMFusion repos
 rpm -ivh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 rpm -ivh https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 ### Repo to install Spotify app
 dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
+
 echo "##### Done #####"
 echo
 
-echo "##### Installing utilities..."
-###	Utilities for installation
-dnf $install gnome-tweak-tool
-dnf $install https://dl.folkswithhats.org/fedora/$(rpm -E %fedora)/RPMS/folkswithhats-release.noarch.rpm
-dnf $install fedy
-dnf $install chrome-gnome-shell
 
-###	Install compilers and utilities
-dnf $install make gcc kmod
-dnf $install kmod-devel*
+echo "##### Installing basic utilities..."
+Install wget
+Install curl
+Install dnf-plugins-core
+Install yumex-dnf
+Install gnome-tweak-tool
+Install https://dl.folkswithhats.org/fedora/$(rpm -E %fedora)/RPMS/fedy-release.rpm
+Install fedy
+Install chrome-gnome-shell
+
+
+###	Install compilers
+Install make gcc kmod
+Install kmod-devel*
+
 
 ###	Install libelf utilities
 ### Installs only if the script is running on Virtual Guests.
 if [[ $vm == 1 ]]; then
-    dnf $install elfutils-libelf-devel*
-    dnf $install elfutils*
+    Install elfutils-libelf-devel*
+    Install elfutils*
 fi
+
 echo "##### Done #####"
 echo
 
+
 echo "##### Activating ssh daemon..."
-###	Activate ssh
 systemctl enable sshd
 systemctl start sshd
 echo "##### Done #####"
 echo
 
 echo "##### Updating the system..."
-###	System update
 dnf $update
 echo "##### Done #####"
 echo
 
+
 ###	Installs only if the script is running on Virtual Guests.
 echo "##### Updating Kernels for virtual machines..."
 if [[ $vm == 1 ]]; then
-    dnf $install kernel*
+    Install kernel*
     echo "##### Done #####"
 fi
+
 
 ### Displaying the status of the services activated
 echo
@@ -113,6 +134,7 @@ case $input in
     * )     echo;;
 esac
 echo "##### Done #####"
+
 
 echo
 echo "********** Configuration and Update are done, please reboot the system **********"
